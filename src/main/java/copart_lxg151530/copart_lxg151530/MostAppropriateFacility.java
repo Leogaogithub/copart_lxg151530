54dc5a831e5a77384b02553a769e99b32c03eedc
Leogaogithub <longhaogao@gmail.com>
2016-12-19 15:15:38 -0600

    UPDATA

--- src/main/java/copart_lxg151530/copart_lxg151530/MostAppropriateFacility.java
+++ src/main/java/copart_lxg151530/copart_lxg151530/MostAppropriateFacility.java
@@ -1,7 +1,36 @@
 package copart_lxg151530.copart_lxg151530;
 
+interface ClosestCopartRule{
+	public String getClosestCopartFacility(String id, String zipcode);
+}
+
+class ClosestCopartRuleBasedOnDB implements ClosestCopartRule{
+	public String getClosestCopartFacility(String id, String zipcode){		
+		return null;
+	}	
+}
+
+class ClosestCopartRuleBasedOnEngine implements ClosestCopartRule{
+	public String getClosestCopartFacility(String id, String zipcode){		
+		return null;
+	}	
+}
+
+class Customer{
+	String id;
+	//assuming default rule is ClosestCopartRuleBasedOnDB
+	ClosestCopartRule rule = new ClosestCopartRuleBasedOnDB();
+	ClosestCopartRule getClosestCopartRule(){
+		return rule;
+	}
+	
+	public void setClosestCopartRule(ClosestCopartRule rule ){
+		this.rule = rule;
+	}
+}
+
 public class MostAppropriateFacility {
 	public static void main(String[] args) {		
-
+		
 	}
 }
